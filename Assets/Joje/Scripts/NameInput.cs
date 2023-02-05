04f882029c1a572f48a0ef844260f5423278426b
joje <joaop.mendes@icloud.com>
2023-02-04 21:10:07 -0300

    Book Prototype

---
+++ Assets/Joje/Scripts/NameInput.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NameInput : MonoBehaviour
+{
+    [Header("Family Member")]
+    public string name;
+    public Image picture;
+    [Header("Input Field")] 
+    public GameObject field;
+    
+    private string input;
+
+    public void ReadStringInput(string s)
+    {
+        input = s;
+        print(input);
+        if (name.ToLower().Equals(input.ToLower()))
+        {
+            field.SetActive(false);
+            print("Disable Image");
+            picture.gameObject.SetActive(true);
+        }
+    }
+}
